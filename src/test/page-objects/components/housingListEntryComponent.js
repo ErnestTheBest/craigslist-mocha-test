ac749eeff7bfa38ad2f801a31cddced2d8697452
Ernest Pakers <pakers.ernest@gmail.com>
2019-05-26 22:31:06 +0300

    Standard code style check global variables added.

--- src/test/page-objects/components/housingListEntryComponent.js
+++ src/test/page-objects/components/housingListEntryComponent.js
@@ -1,5 +1,6 @@
 module.exports = class ListEntry {
   constructor (entryElement) {
+    // This text is not used anywhere yet, just for the sake of it
     this.entryText = entryElement.$('.result-row .result-title.hdrlnk').getText()
     // I left . in numbers just in case there are some 123,456.78 numbers
     this.entryPrice = parseFloat(entryElement.$('.result-row .result-meta .result-price')
